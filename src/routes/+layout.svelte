09624ebeb3c6075aa578116de31e1836ac9b47db
Zac M-W <contact@zac.nz>
2025-01-22 20:46:26 +0000

    Fix socials

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,4 +19,16 @@
   });
 </script>
 
+<svelte:head>
+  <title>IDE Dashboard</title>
+  <meta property="og:title" content="IDE Dashboard" />
+  <meta property="og:image" content="/cover.jpg" />
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="IDE Dash" />
+  <link rel="manifest" href="/site.webmanifest" />
+</svelte:head>
+
 {@render children()}
